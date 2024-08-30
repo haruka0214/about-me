3d4d711a1562570f999fbbb4fc2cbf3735e8c009
yukikimoto <167736996+jeje0730@users.noreply.github.com>
2024-08-30 11:56:59 +0900

    ページ作成

--- js/scripts.js
+++ js/scripts.js
@@ -14,35 +14,27 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
 
-//テキストのカウントアップ+バーの設定
-var bar = new ProgressBar.Line(splash_text, {//id名を指定
-    easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-    duration: 1000,//時間指定(1000＝1秒)
-    strokeWidth: 0.2,//進捗ゲージの太さ
-    color: '#555',//進捗ゲージのカラー
-    trailWidth: 0.2,//ゲージベースの線の太さ
-    trailColor: '#bbb',//ゲージベースの線のカラー
-    text: {//テキストの形状を直接指定
-      style: {//天地中央に配置
-        position: 'absolute',
-        left: '50%',
-        top: '50%',
-        padding: '0',
-        margin: '-30px 0 0 0',//バーより上に配置
-        transform:'translate(-50%,-50%)',
-        'font-size':'1rem',
-        color: '#fff',
-      },
-      autoStyleContainer: false //自動付与のスタイルを切る
-    },
-    step: function(state, bar) {
-      bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
-    }
-  });
+    document.addEventListener('DOMContentLoaded', () => {
+      const starfield = document.querySelector('.starfield');
+      const starCount = 50;
   
-  //アニメーションスタート
-  bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 で100%まで描画します
-    $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+      for (let i = 0; i < starCount; i++) {
+          const star = document.createElement('div');
+          star.classList.add('star');
+          
+          // 星のサイズと位置を設定
+          const size = Math.random() * 20 + 10; // 5px〜15pxのサイズ
+          const xPosition = Math.random() * 100 + 'vw';
+          const animationDuration = Math.random() * 5 + 5; // 5〜10秒のアニメーション
+  
+          star.style.width = `${size}px`;
+          star.style.height = `${size}px`;
+          star.style.left = xPosition;
+          star.style.animation = `fall ${animationDuration}s linear infinite`;
+  
+          starfield.appendChild(star);
+      }
   });
 
-  //スクロールした際の動きを関数でまとめる
+  
+  
